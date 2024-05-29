4196cdc24404c44c3bdded1e0fdcea7414b03fda
Lubna5Sh <eaa24las@students.eaaa.dk>
2024-05-29 14:08:19 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,1 +1,10 @@
+$('#view-flasker').on('click', function() {
+    const flasker = $('#flasker').position().top;
 
+    $('html, body').animate(
+        {
+            scrollTop: flasker
+        },
+        900
+    );
+});
