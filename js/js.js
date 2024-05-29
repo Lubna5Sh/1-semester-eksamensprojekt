986cbd50f5f34859fafe996de4a86fb82fa955cc
Lubna5Sh <eaa24las@students.eaaa.dk>
2024-05-29 14:51:43 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -8,3 +8,27 @@ $('#view-flasker').on('click', function() {
         900
     );
 });
+
+
+$('#view-daaser').on('click', function() {
+    const flasker = $('#daaser').position().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: daaser
+        },
+        900
+    );
+});
+
+
+$('#view-tilbud').on('click', function() {
+    const tilbud = $('#tilbud').position().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: tilbud
+        },
+        900
+    );
+});
